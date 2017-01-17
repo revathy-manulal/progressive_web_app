00a349de4fa0c77dbb664ae3b019dd5e4947f669
Revathy Manulal <revathymanulal@hotmail.com>
2017-01-18 00:18:15 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -53,7 +53,7 @@ self.addEventListener('fetch', function(e) {
   console.log('[ServiceWorker] Fetch', e.request.url);
   e.respondWith(
     caches.match(e.request).then(function (response) {
-      return response || fetch.(e.request);
+      return response || fetch(e.request);
     })
   );
 });
